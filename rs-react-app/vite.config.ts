5aa34b22a8500d70f5c06440886a178cbe63e626
Elena-MyOne <yakovenko.ea72@gmail.com>
2025-07-14 20:08:31 -0400

    feat: setup tests

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      exclude: [
+        './.eslintrc.cjs',
+        './vite.config.ts',
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+    },
+  },
+  base: './',
 });
