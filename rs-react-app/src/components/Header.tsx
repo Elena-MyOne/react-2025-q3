d8c477fbd189d36bffc2237f15b9c0447d395a78
Elena-MyOne <yakovenko.ea72@gmail.com>
2025-07-17 14:53:00 -0400

    feat: create custom hook and convert header class component into functional

--- rs-react-app/src/components/Header.tsx
+++ rs-react-app/src/components/Header.tsx
@@ -1,75 +1,52 @@
-import React from 'react';
 import { CiSearch } from 'react-icons/ci';
 import { LOCAL_STORAGE_VALUE } from '../consts';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 interface HeaderProps {
   value: string;
   handleSearch(): Promise<void>;
 }
 
-export default class Header extends React.Component<
-  HeaderProps,
-  { value: string }
-> {
-  constructor(props: HeaderProps) {
-    super(props);
-    this.state = {
-      value: props.value || '',
-    };
+export default function Header({ handleSearch }: HeaderProps) {
+  const [value, setValue] = useLocalStorage(LOCAL_STORAGE_VALUE, '');
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSearchButton = this.handleSearchButton.bind(this);
-    this.handleSearchForm = this.handleSearchForm.bind(this);
-  }
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
 
-  componentDidMount(): void {
-    const savedValue = localStorage.getItem(LOCAL_STORAGE_VALUE) || '';
-    this.setState({ value: savedValue });
-  }
-
-  handleChange(event: React.FormEvent<HTMLInputElement>): void {
-    const target = event.currentTarget.value;
-    this.setState({ value: target });
-  }
-
-  handleSearchButton(): void {
-    const { value } = this.state;
+  const handleSearchButton = () => {
     localStorage.setItem(LOCAL_STORAGE_VALUE, value);
-  }
+  };
 
-  handleSearchForm(event: React.FormEvent) {
+  const handleSearchForm = (event: React.FormEvent) => {
     event.preventDefault();
-    const { handleSearch } = this.props;
-    this.handleSearchButton();
+    handleSearchButton();
     handleSearch();
-  }
+  };
 
-  render() {
-    const { value } = this.state;
-
-    return (
-      <header className="flex m-auto gap-4 border-b border-gray-200 p-0 py-4 md:container justify-between items-center">
-        <div className="font-bold text-primary text-xl text-green-500">
-          Rick And Morty
+  return (
+    <header className="flex m-auto gap-4 border-b border-gray-200 p-0 py-4 md:container justify-between items-center">
+      <div className="font-bold text-primary text-xl text-green-500">
+        Rick And Morty
+      </div>
+      <form onSubmit={handleSearchForm} data-testid="search-form">
+        <div className="flex items-center">
+          <input
+            type="text"
+            className="grow border-gray-300 border-[1px] p-2"
+            placeholder="Search..."
+            value={value}
+            onChange={handleChange}
+          />
+          <button
+            type="submit"
+            className="text-2xl cursor-pointer text-black border-[1px] border-transparent bg-green-400 hover:bg-green-500 p-2"
+            onClick={handleSearchButton}
+          >
+            <CiSearch />
+          </button>
         </div>
-        <form onSubmit={this.handleSearchForm} data-testid="search-form">
-          <div className="flex items-center">
-            <input
-              type="text"
-              className="grow border-gray-300 border-[1px] p-2"
-              placeholder="Search..."
-              value={value}
-              onChange={this.handleChange}
-            />
-            <button
-              className="text-2xl cursor-pointer text-black duration-300 border-[1px] border border-transparent bg-green-400 hover:bg-green-500 duration-300 p-2"
-              onClick={this.handleSearchButton}
-            >
-              <CiSearch />
-            </button>
-          </div>
-        </form>
-      </header>
-    );
-  }
+      </form>
+    </header>
+  );
 }
