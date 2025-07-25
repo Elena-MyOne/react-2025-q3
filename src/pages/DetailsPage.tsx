43562065d6896c8498e3028adde43c2e49ca0da8
Elena-MyOne <yakovenko.ea72@gmail.com>
2025-07-25 09:29:29 -0400

    feat: add details page content

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,19 +1,76 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { ROUTE_PATHS } from '../routes';
+import { useCallback, useEffect, useState } from 'react';
+import { BASE_URL } from '../consts';
+import Loader from '../components/Loader';
+import type { CharacterData } from '../models/interfaces';
 
 export default function DetailsPage() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [character, setCharacter] = useState<CharacterData | null>(null);
+
+  const getCharacterById = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${BASE_URL}/${id}`);
+
+      if (!response.ok) {
+        throw new Error('Network response error');
+      }
+
+      const data = await response.json();
+      setCharacter(data);
+      setIsLoading(false);
+      console.log(data);
+    } catch (error) {
+      console.error('Cannot get character by id', error);
+      setIsLoading(false);
+      setErrorMessage(`Cannot get character by id. Error: ${error}`);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    getCharacterById();
+  }, [getCharacterById, id]);
 
   return (
-    <div className="w-full my-5 text-center bg-gray-100">
-      <div className="">Details {id}</div>
-      <button
-        className="text-center cursor-pointer text-black duration-300 border-[1px] border border-transparent bg-green-400 hover:bg-green-500 p-2 my-4"
-        onClick={() => navigate(`${ROUTE_PATHS.HOME}`)}
-      >
-        Close details
-      </button>
+    <div className="bg-white text-center mt-4">
+      {isLoading ? (
+        <div className="min-w-[300px]">
+          <Loader />
+        </div>
+      ) : (
+        <>
+          {character && (
+            <div className="shadow-md border ">
+              <figure className=" flex items-center justify-center w-[300px] h-[300px]">
+                <img
+                  src={character.image}
+                  alt={`character ${character.image} image`}
+                />
+              </figure>
+              <h3 className="py-4 font-bold pb-2">{character.name}</h3>
+              <p>Gender: {character.gender}</p>
+              <p>Species: {character.species}</p>
+              <p>Status: {character.status}</p>
+              <p>Type: {character.type ? character.type : 'unknown'}</p>
+              <p>Gender: {character.gender ? character.gender : 'unknown'}</p>
+              <button
+                className="text-center cursor-pointer text-black duration-300 border-[1px] border border-transparent bg-green-400 hover:bg-green-500 p-2 my-4"
+                onClick={() => navigate(`${ROUTE_PATHS.HOME}`)}
+              >
+                Close details
+              </button>
+            </div>
+          )}
+          {errorMessage && (
+            <div className="text-red-600">Character not found</div>
+          )}
+        </>
+      )}
     </div>
   );
 }
