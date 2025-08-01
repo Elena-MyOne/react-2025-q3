bc6ac1168857156a7568d98e6e87b6cb9e45f6d9
Elena-MyOne <yakovenko.ea72@gmail.com>
2025-07-31 21:44:12 -0400

    feat: add test for select items icon into character card component

--- src/__tests__/CharacterCard.test.tsx
+++ src/__tests__/CharacterCard.test.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import CharacterCard from '../components/CharacterCard';
 import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { mockCharacter } from '../mocks/mockCharacter';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import { ThemeProvider } from '../theme/ThemeProvider';
+import { selectSelectedItems } from '../redux/slices/selectedItemsSlice';
 
 const MockCharacterCard = () => {
   return (
@@ -35,5 +36,21 @@ describe('CharacterCard component', () => {
     expect(cardName).toBeInTheDocument();
     expect(gender).toBeInTheDocument();
     expect(species).toBeInTheDocument();
+  });
+
+  it('adds and removes character from selectedItems on heart icon click', () => {
+    render(<MockCharacterCard />);
+    const icon = screen.getByTestId('icon');
+
+    let currentSelected = selectSelectedItems(store.getState()).selectedItems;
+    expect(currentSelected).not.toContainEqual(mockCharacter);
+
+    fireEvent.click(icon);
+    currentSelected = selectSelectedItems(store.getState()).selectedItems;
+    expect(currentSelected).toContainEqual(mockCharacter);
+
+    fireEvent.click(icon);
+    currentSelected = selectSelectedItems(store.getState()).selectedItems;
+    expect(currentSelected).not.toContainEqual(mockCharacter);
   });
 });
